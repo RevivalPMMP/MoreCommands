ebc5678d1718affb4601447298951e2aee64abec
Lemuel <lemmy.casanova@gmail.com>
2015-07-31 23:49:45 -0400

    Improve code readability, implement CommandExecutor

--- src/GlaciercreepsMC/morecommands/manager/FreezeManager.php
+++ src/GlaciercreepsMC/morecommands/manager/FreezeManager.php
@@ -7,49 +7,111 @@ use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerMoveEvent;
 use pocketmine\event\block\BlockBreakEvent;
 use pocketmine\Player;
+use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\command\CommandExecutor;
 use pocketmine\utils\TextFormat;
 
-class FreezeManager implements Listener {
+class FreezeManager implements Listener, CommandExecutor {
     
     public $plugin;
     public $frozen = [];
+    private $permMessage;
+    
     
     public function __construct(PluginBase $plugin) {
         $this->plugin = $plugin;
         $this->plugin->getServer()->getPluginManager()->registerEvents($this, $this->plugin);
+        $this->permMessage = TextFormat::RED."You do not have permission for this!";
+    }
+    
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args) {
+        $count = count($args);
+        if ($sender->hasPermission("morecommands.freeze")) {
+            
+            if ($count == 0) {
+                return false;
+            }
+            if ($count == 1) {
+                $target = $this->getServer()->getPlayer($args[0]);
+
+                if ($target == null) {
+                    $sender->sendMessage(TextFormat::YELLOW."Player '".
+                        TextFormat::AQUA.$args[0].TextFormat::YELLOW."' was not found!");
+                    return true;
+                } else {
+                    $this->freezePlayer($target, $sender);
+                    return true;
+                }
+                        
+            }
+        } else {
+            $sender->sendMessage($this->permMessage);
+            return true;
+        }
+
+        
+        if ($sender->hasPermission("morecommands.unfreeze")){
+            
+            if ($count == 0){
+                return false;
+            }
+            
+            if ($count == 1){
+                $target = $this->getServer()->getPlayer($args[0]);
+
+                if ($target == null){
+                    $sender->sendMessage(TextFormat::YELLOW."Player '".
+                        TextFormat::AQUA.$args[0].TextFormat::YELLOW."' was not found!");
+                    return true;
+                } else {
+                    $this->unfreezePlayer($target, $sender);
+                    return true;
+                }
+        
+            }
+            
+         } else {
+            $sender->sendMessage($this->permMessage);
+            return true;
+        }
     }
     
     public function freezePlayer(Player $player, CommandSender $sender){
-        $id = $player->getID();
+        $id = $player->getUniqueId();
         $name = $player->getName();
         
         if (in_array($id, $this->frozen)){
-            $sender->sendMessage(TextFormat::YELLOW."Player ".
-                TextFormat::AQUA.$name.TextFormat::YELLOW." is already frozen!");
+            $sender->sendMessage(TextFormat::YELLOW."Player '".
+                TextFormat::AQUA.$name.TextFormat::YELLOW."' is already frozen!");
         } else {
             $this->frozen[$name] = $id;
-            $sender->sendMessage(TextFormat::GREEN."Player ".
-                TextFormat::AQUA.$name.TextFormat::GREEN." is now frozen.");
+            $sender->sendMessage(TextFormat::GREEN."Player '".
+                TextFormat::AQUA.$name.TextFormat::GREEN."' is now frozen.");
             $player->sendMessage(TextFormat::AQUA."You have been frozen.");
         }
+        
     }
     
     public function unfreezePlayer(Player $player, CommandSender $sender){
-        $id = $player->getID();
+        $id = $player->getUniqueId();
         $name = $player->getName();
+        
         if (in_array($id, $this->frozen)){
             $index = array_search($id, $this->frozen);
+            
             if ($index === false){
-                $sender->sendMessage(TextFormat::YELLOW."Player ".
-                    TextFormat::AQUA.$name.TextFormat::YELLOW." wasn't frozen!");
+                $sender->sendMessage(TextFormat::YELLOW."Player '".
+                    TextFormat::AQUA.$name.TextFormat::YELLOW."' wasn't frozen!");
             } else {
                 unset($this->frozen[$index]);
-                $sender->sendMessage(TextFormat::GREEN."Player ".
-                        TextFormat::AQUA.$name.TextFormat::GREEN." has been unfrozen.");
+                $sender->sendMessage(TextFormat::GREEN."Player '".
+                        TextFormat::AQUA.$name.TextFormat::GREEN."' has been unfrozen.");
                 $player->sendMessage(TextFormat::GREEN."You have been unfrozen.");
             }
+            
         }
+        
     }
     
     public function getFrozenPlayers(){
@@ -60,7 +122,7 @@ class FreezeManager implements Listener {
     {
         $player = $event->getPlayer();
         foreach ($this->frozen as $name => $id){
-            if ($player->getName() === $name && $player->getId() === $id){
+            if ($player->getName() === $name && $player->getUniqueId() === $id){
                 $event->setTo($event->getFrom());
             }
         }
@@ -69,9 +131,10 @@ class FreezeManager implements Listener {
     public function onBlockBreak(BlockBreakEvent $event){
         $player = $event->getPlayer();
         foreach ($this->frozen as $name => $id){
-            if ($player->getName() === $name && $player->getId() === $id){
+            if ($player->getName() === $name && $player->getUniqueId() === $id){
                 $event->setCancelled();
             }
         }
     }
+
 }
