b9e060d45ac49c65d3a266555388a10503c4dd28
Lemuel Casanova <lemmy.casanova@gmail.com>
2015-10-17 16:31:33 -0400

    Change order of parameters slightly, for consistency; change desc and useMessage to have no default; give consoleMessage a default

--- src/GlaciercreepsMC/morecommands/base/BaseCommand.php
+++ src/GlaciercreepsMC/morecommands/base/BaseCommand.php
@@ -1,5 +1,6 @@
 <?php
 namespace GlaciercreepsMC\morecommands\base;
+use GlaciercreepsMC\morecommands\MoreCommands;
 use pocketmine\command\Command;
 use pocketmine\command\PluginIdentifiableCommand;
 use pocketmine\utils\TextFormat;
@@ -7,14 +8,21 @@ abstract class BaseCommand extends Command implements PluginIdentifiableCommand 
 
 	private $plugin;
 
-	private $consoleMsg = TextFormat::RED. "Silly console, this command is for in-game!";
+	private $consoleMsg;
 	private $usageMessage;
 
-	public function __construct(MoreCommands $plugin, $name, $desc = "", $useMessage = null, $consoleUsageMessage = null, array $aliases = []){
+	public function __construct(Moreommands $plugin, $name, $desc, $useMessage, $consoleUsageMessage = null, array $aliases = []){
 		parent::__construct($name, $desc, $useMessage, $aliases);
 		$this->plugin = $plugin;
 		$this->usageMessage = $useMessage;
-		$this->consoleUsageMessage = $consoleUsageMessage;
+		
+		//if set, use the console message sent as an argument,
+		//otherwise use the default in the else clause
+		if ($consoleUsageMessage !== null){
+			$this->consoleMsg = $consoleUsageMessage;
+		} else {
+			$this->consoleMsg = TextFormat::RED."Silly console, this command is for in-game!";
+		}
 	}
 
 	public function getPlugin(){
