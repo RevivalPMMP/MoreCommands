c95685d0ddc9b99bdbc11aa2fa17b78b4ccb9735
RandomAltThingy <goldowl37@gmail.com>
2015-10-17 08:18:13 +0100

    Changed format of commands

--- src/GlaciercreepsMC/morecommands/commands/CommandHeal.php
+++ src/GlaciercreepsMC/morecommands/commands/CommandHeal.php
@@ -3,38 +3,45 @@ namespace GlaciercreepsMC\morecommands\commands;
 use GlaciercreepsMC\morecommands\base\BaseCommand;
 use pocketmine\utils\TextFormat as Color;
 use pocketmine\Player;
-class CommandHeal extends BaseCommand {
-
-	public function __construct(MoreCommands $plugin){
-        parent::__construct($plugin, "heal", "Heal you or someone else", "/heal [player]", null, ["null"]);
+class Heal extends BaseCommand {
+    private $plugin;
+    public function __construct(MoreCommands $plugin){
+        parent::__construct($plugin, "heal", "Heal yourself or other player", "/heal [player]");
         $this->setPermission("morecommands.heal");
+        $this->plugin = plugin;
     }
-
-	public function execute(CommandSender $sender, $alias, array $args){
-		if(!$this->testPermission($sender)){
-			$sender->sendMessage(Color::RED. "You dont have permission to heal!");
-			return false;
-		}
-		if($sender instanceof Player){
-			if(count($args) == 0){
-				$sender->setHealth(20);
-				$sender->sendMessage(Color::GREEN. "You have been healed!");
-				return true;
-			}
-			if(count($args) >= 2){
-				$sender->sendMessage(Color::RED. "Too many arguments!"):
-				$sender->sendMessage(Color::RED. "Usage: /heal [player]");
-				return false;
-			}
-		}
-		if(count($args) == 1){
-			$player = $this->getPlugin()->getServer()->getPlayer($args[1]);
-
-			$player->setHealth(20);
-			$player->sendMessage(Color::GREEN. "You have been healed by ". Color::YELLOW. $sender->getName());
-			$sender->sendMessage(Color::YELLOW. $player->getName(). Color::GREEN. " has been healed!");	
-			return true;
-		}
-	}
-
+    public function execute(CommandSender $sender, $alias, array $args){
+        if(!$this->testPermission($sender)){
+            return false;
+        }
+        switch(count($args)){
+            case 0:
+                if(!$sender instanceof Player){
+                    $sender->sendMessage(Color::RED. "Usage: /heal <player>");
+                    return false;
+                }
+                $sender->setHealth(20);
+                $sender->sendMessage(Colot::GREEN . "You have been healed!");
+                break;
+            case 1:
+                if(!$sender->hasPermission("morecommands.heal.other")){
+                    $sender->sendMessage(Colot::RED . "You do not have permission to heal others");
+                    return false;
+                }
+                $player = $this->plugin->getServer()->getPlayer($args[0]);
+                if(!$player){
+                    $sender->sendMessage(TextFormat::RED . "[Error] Player not found");
+                    return false;
+                }
+                $player->setHealth(20);
+                $sender->sendMessage(Color::GREEN . $player->getDisplayName() . " has been healed!");
+                $player->sendMessage(Color::GREEN . "You have been healed!");
+                break;
+            default:
+                $sender->sendMessage($sender instanceof Player ? "Usage: /heal [player]" : "Please use this command in-game");
+                return false;
+                break;
+        }
+        return true;
+    }
 }
