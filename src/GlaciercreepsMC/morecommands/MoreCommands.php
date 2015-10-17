6097751a6193cd3fe30a87f8e06637f8cee244ba
Lemuel Casanova <lemmy.casanova@gmail.com>
2015-10-17 12:53:46 -0400

    Fix formatting to tabs, and a couple typos

--- src/GlaciercreepsMC/morecommands/MoreCommands.php
+++ src/GlaciercreepsMC/morecommands/MoreCommands.php
@@ -8,15 +8,15 @@ class MoreCommands extends PluginBase implements Listener {
 	public function onEnable(){
 		//Register events
 		$pm = $this->getServer()->getPluginManager();
-		$pm->registerEvents($this, $this): //TODO change
+		$pm->registerEvents($this, $this); //TODO change
 		
 		//Register commands
 		$this->registerCommands();
 	}
 	
 	public function registerCommands(){
-      //TODO check if correct code, I forgot -_-
-       // $cm = $this->getServer()->getCommandManager();
+		//TODO check if correct code, I forgot -_-
+		// $cm = $this->getServer()->getCommandManager();
 		//$cm->registerCommand("heal", new CommandHeal($this));
 	}
 	
--- src/GlaciercreepsMC/morecommands/commands/CommandHeal.php
+++ src/GlaciercreepsMC/morecommands/commands/CommandHeal.php
@@ -5,19 +5,19 @@ use pocketmine\utils\TextFormat;
 use pocketmine\Player;
 class CommandHeal extends BaseCommand {
 	
-    private $plugin;
+	private $plugin;
 	
-    public function __construct(MoreCommands $plugin){
-        parent::__construct($plugin, "heal", "Heal yourself or other player", "/heal [player]");
-        $this->setPermission("morecommands.heal");
-    }
+	public function __construct(MoreCommands $plugin){
+		parent::__construct($plugin, "heal", "Heal yourself or other player", "/heal [player]");
+		$this->setPermission("morecommands.heal");
+	}
 	
-    public function execute(CommandSender $sender, $alias, array $args){
-        if(!$this->testPermission($sender)){
-            return false;
-        }
-        switch(count($args)){
-            case 0:
+	public function execute(CommandSender $sender, $alias, array $args){
+		if(!$this->testPermission($sender)){
+			return false;
+		}
+		switch(count($args)){
+			case 0:
 				if (!($sender instanceof Player)){
 					$sender->sendMessage(TextFormat::RED."Silly console! Try /heal <player>");
 					return true;
@@ -26,8 +26,8 @@ class CommandHeal extends BaseCommand {
 					$sender->sendMessage(TextFormat::GREEN."You have been healed");
 					return true;
 				}
-            case 1:
-                $target = $this->plugin->getServer()->getPlayer($args[0]);
+			case 1:
+				$target = $this->plugin->getServer()->getPlayer($args[0]);
 				if ($target == null){
 					$sender->sendMessage(TextFormat::RED. "That player cannot be found");
 					return true;
@@ -37,12 +37,11 @@ class CommandHeal extends BaseCommand {
 					$sender->sendMessage(TextFormat::YELLOW."Player ". $target. " was healed");
 					return true;
 				}
-            default:
-                $sender->sendMessage($sender instanceof Player ? "Usage: /heal [player]" : "/heal <player>");
-                return false;
-                break;
-        }
-        return true;
-    }
+			default:
+				$sender->sendMessage($sender instanceof Player ? "Usage: /heal [player]" : "/heal <player>");
+				return false; //break; is unnecessary because return won't let php reach that code
+		}
+		return true;
+	}
 	
 }
