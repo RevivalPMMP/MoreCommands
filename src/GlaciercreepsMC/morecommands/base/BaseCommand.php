0c809e8f311b3df6be3f4c6fc7772bb6fc0a53ed
Lemuel Casanova <lemmy.casanova@gmail.com>
2015-11-02 21:16:14 -0500

    Add doc comments, esp. for autocomplete with ->getPlugin()

--- src/GlaciercreepsMC/morecommands/base/BaseCommand.php
+++ src/GlaciercreepsMC/morecommands/base/BaseCommand.php
@@ -24,13 +24,33 @@ abstract class BaseCommand extends Command implements PluginIdentifiableCommand 
 			$this->consoleMsg = TextFormat::RED."Silly console, this command is for in-game!";
 		}
 	}
-
+	
+	/**
+	 * Gets a MoreCommands (PluginBase) object
+	 * 
+	 * @return MoreCommands     
+	 */
 	public function getPlugin(){
 		return $this->plugin;
 	}
 
+	/**
+	 * Gets the command usage
+	 * 
+	 * @return string     The usage of the current command
+	 */
 	public function getUsage(){
 		$this->usageMessage;
+	}
+	
+	/**
+	 * Gets the error message to be sent to the console if the current command
+	 * is to be used in-game.
+	 * 
+	 * @return string     The message to be sent to the console
+	 */	
+	public function getConsoleUsageMessage(){
+		return $this->consoleMsg;
 	}
 
 }
